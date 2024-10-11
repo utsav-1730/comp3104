eb9ac460521159cce9222b3f1350030bc22db785
Utsav <utsavchanganiedu@gmail.com>
2024-10-11 19:47:40 -0400

    title updated

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 10147134 || Utsav Changani || DevOps</h5>
+      <h5>Developed By: 10147134 || Utsav Changani || DevOps - COMP3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
